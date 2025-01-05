e7badc0b13b0479d61f0cef99af82f7e6a134fe8
rivaldosetyo <mrivaldosetyo@gmail.com>
2025-01-05 21:02:27 +0700

    Add new chart (line chart)

--- my-chart-app/src/app/page.js
+++ my-chart-app/src/app/page.js
@@ -1,14 +1,10 @@
-//import { useEffect, useState } from "react";
-import Image from "next/image";
-import ChartComponent from '@/components/ui/chartExample';
 import ChartPage from "@/components/ChartPage";
 import ChartPage2 from "@/components/ChartPage2";
+import ChartPage3 from "@/components/ChartPage3";
 import { ProcessKlinikData } from "@/lib/processData";
 import {
   Card,
   CardContent,
-  CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
@@ -19,10 +15,12 @@ export default function Home() {
     <main className="p-8">
       <h1 className="text-2xl font-bold mb-4">Stacked Chart Example</h1>
       <div style={{ display: 'flex', alignItems: 'flex-start', width: '100vw' }}> {/* Menggunakan Flexbox */}
-        <div style={{width: '50%'}} >
+        <div style={{ width: '50%' }} >
           <ChartPage chartData={data} /> {/* Mengirim data ke ChartPage */}
-          <br/>
+          <br />
           <ChartPage2 chartData={data} /> {/* Mengirim data ke ChartPage */}
+          <br />
+          <ChartPage3 chartData={data} /> {/* Mengirim data ke ChartPage */}
         </div>
         <Card style={{ marginLeft: '20px' }}> {/* Tambahkan margin untuk jarak */}
           <CardHeader>
@@ -30,32 +28,32 @@ export default function Home() {
           </CardHeader>
           <CardContent>
             <label>
-              <input type="checkbox" /> {/* Checkbox */}
+              <input value="Gross Profit" type="checkbox" /> {/* Checkbox */}
               Gross Profit
             </label>
             <br />
             <label>
-              <input type="checkbox" /> {/* Checkbox */}
+              <input value="Debt Repayment" type="checkbox" /> {/* Checkbox */}
               Debt Repayment
             </label>
             <br />
             <label>
-              <input type="checkbox" /> {/* Checkbox */}
+              <input value="Indirect Expense" type="checkbox" /> {/* Checkbox */}
               Indirect Expense
             </label>
             <br />
             <label>
-              <input type="checkbox" /> {/* Checkbox */}
+              <input value="Equipment Procurement" type="checkbox" /> {/* Checkbox */}
               Equipment Procurement
             </label>
             <br />
             <label>
-              <input type="checkbox" /> {/* Checkbox */}
+              <input value="Fit Out" type="checkbox" /> {/* Checkbox */}
               Fit Out
             </label>
             <br />
             <label>
-              <input type="checkbox" /> {/* Checkbox */}
+              <input value="Cumulative Cashflow" type="checkbox" /> {/* Checkbox */}
               Cumulative Cashflow
             </label>
           </CardContent>
---
+++ my-chart-app/src/components/ChartPage3.tsx
@@ -0,0 +1,83 @@
+/* eslint-disable */
+"use client";
+
+import { TrendingUp } from "lucide-react";
+import {
+  Line,
+  LineChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  ComposedChart,
+  Rectangle
+} from "recharts";
+import { ProcessKlinikData } from "@/lib/processData";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+
+const chartConfig = {
+  total_revenue_expense: {
+    label: "Comulative Cashflow",
+    color: "hsl(var(--chart-1))",
+  }
+} satisfies ChartConfig;
+
+const colors = ["#8884d8", "#82ca9d", "#ffc658", "#ff7300", "#ff0000"];
+
+export default function ChartPage({ chartData }) {
+  console.info(chartData);
+  return (
+    <Card style={{ width: '100%' }}>
+      <CardHeader>
+        <CardTitle>Line Chart - Revenue and Expense</CardTitle>
+        <CardDescription></CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={chartConfig}>
+          <LineChart data={chartData}>
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="month"
+              tickLine={false}
+              tickMargin={10}
+              axisLine={false}
+            />
+            <YAxis domain={["auto", "auto"]} allowDataOverflow />
+            <Tooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Line
+              type="monotone"
+              dataKey="comulative"
+              stroke="#8884d8" // Warna garis
+              strokeWidth={2}
+              dot={{ stroke: '#8884d8', strokeWidth: 2 }} // Gaya titik
+              activeDot={{ r: 8 }} // Titik aktif
+            />
+          </LineChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter className="flex-col items-start gap-2 text-sm">
+      </CardFooter>
+    </Card>
+  );
+}
